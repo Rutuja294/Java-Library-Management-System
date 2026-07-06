75ac5b419a628cb20b5cd53e115e24a8c85addeb
Rutuja Patil <patilrutuja294@gmail.com>
2026-07-06 15:05:23 +0530

    Project Completed

--- src/LibraryManagementSystem/Library.java
+++ src/LibraryManagementSystem/Library.java
@@ -56,7 +56,7 @@ public class Library {
 		int bookId=sc.nextInt();
 		boolean found=false;
 		for(int i=0;i<count;i++) {
-			if(books[i].getBookId()==bookId) {
+			if(books[i].getBookId()==bookId){
 				
 				System.out.println("Book Found");
 				System.out.println("Details: ");
@@ -74,6 +74,104 @@ public class Library {
 		if(!found) {
 			System.out.println("Book Not Found");
 		}
+	}
+	
+	public void issueBook() {
+		System.out.println("Enter Book Id: ");
+		int bookId=sc.nextInt();
+		boolean found=false;
+		for(int i=0;i<count;i++) {
+			if(books[i].getBookId()==bookId) {
+				found=true;
+				if(books[i].isIssued()==true) {
+					System.out.println("Book Is Already Issued");
+					break;
+				}
+				else {
+					books[i].setIssued(true);
+					System.out.println("Book Issued SucessFully");
+					
+					break;
+				}
+			}
+		}
+		if(!found) {
+			System.out.println("Book Not Found");
+		}
+		
+		
+	}
+	
+	public void returnBook() {
+		System.out.println("Enter BookId: ");
+		int bookId=sc.nextInt();
+		boolean found=false;
+		for(int i=0;i<count;i++) {
+			if(books[i].getBookId()==bookId) {
+				found=true;
+				if(books[i].isIssued()==true) {
+					books[i].setIssued(false);
+					System.out.println("Book Returned Successfully");
+					break;
+				}
+				else {
+					System.out.println("Book Already Returned");
+					break;
+				}
+			}
+		}
+		if(!found) {
+			System.out.println("Book Not Found");
+		}
+	}
+	
+	public void removeBook() {
+	    System.out.println("Enter Book ID: ");
+	    int bookId = sc.nextInt();
+
+	    boolean found = false;
+
+	    for (int i = 0; i < count; i++) {
+
+	        if (books[i].getBookId() == bookId) {
+
+	            found = true;
+
+	           
+	            for (int j = i; j < count - 1; j++) {
+	                books[j] = books[j + 1];
+	            }
+
+	            
+	            books[count - 1] = null;
+
+	            count--;
+
+	            System.out.println("Book Removed Successfully.");
+	            break;
+	        }
+	    }
+
+	    if (!found) {
+	        System.out.println("Book Not Found.");
+	    }
+	}
+	
+	public void statistics() {
+		int issuedBooks = 0;
+		int availableBooks = 0;
+		System.out.println("Total books: "+count);
+		for(int i=0;i<count;i++) {
+			if(books[i].isIssued()==true) {
+				issuedBooks++;
+			}
+			else {
+				availableBooks++;
+			}
+		}
+		
+		System.out.println("Issued Books: "+issuedBooks);
+		System.out.println("Available Books: "+availableBooks);
 	}
 
 }
--- src/LibraryManagementSystem/Main.java
+++ src/LibraryManagementSystem/Main.java
@@ -14,7 +14,11 @@ public class Main {
 		    System.out.println("1. Add Book");
 		    System.out.println("2. View Books");
 		    System.out.println("3. Search Book");
-		    System.out.println("4. Exit");
+		    System.out.println("4. Issue Book");
+		    System.out.println("5. Return Book");
+		    System.out.println("6. Remove Book");
+		    System.out.println("7. Statistics");
+		    System.out.println("8. Exit");
 
 		    System.out.print("Enter Choice : ");
 		    choice = sc.nextInt();
@@ -32,9 +36,19 @@ public class Main {
 		    	library.searchBook();
 		    	break;
 		    
-		        
-
 		    case 4:
+		    	library.issueBook();
+		    	break;
+		    case 5:
+		    	library.returnBook();
+		    	break;
+		    case 6:
+		    	library.removeBook();
+		    	break;
+		    case 7:
+		    	library.statistics();
+		    	break;
+		    case 8:
 		        System.out.println("Thank You!");
 		        break;
 
@@ -42,7 +56,7 @@ public class Main {
 		        System.out.println("Invalid Choice");
 		    }
 
-		} while(choice != 4);
+		} while(choice != 5);
 	}
 }
 
